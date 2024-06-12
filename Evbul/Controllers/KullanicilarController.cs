420da710f5e1085425cdb6a4aec3671f44d2b82f
Ahmet Ağgöz <79806216+ahmetaggoz@users.noreply.github.com>
2024-06-12 20:28:40 +0300

    kullanıcılar controller olusturuldu

---
+++ Evbul/Controllers/KullanicilarController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Evbul.Controllers;
+
+public class KullanicilarController:Controller
+{
+    public KullanicilarController()
+    {
+        
+    }
+
+    public IActionResult Giris()
+    {
+        return View();
+    }
+}
